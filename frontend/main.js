1a732ce646da2f99bc91d06fb28c880c48de10d0
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2024-11-08 15:13:43 -0300

    gráfico adicionado e colunas ajustadas

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,24 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-// import Chart from 'chart.js/auto';
 import './assets/css/style.css';
-
+import Chart from 'chart.js/auto';
+const ctx = document.getElementById('myChart');
+  
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
