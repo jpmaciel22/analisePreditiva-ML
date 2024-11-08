60501d5927b87ad808c656ec4a866d4a7fe9c121
jpmaciel22 <131266912+jpmaciel22@users.noreply.github.com>
2024-11-08 15:42:55 -0300

    prototipo da pagina da API feito.

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const apiController = require('./src/controllers/apiController')
 // const { loginRequired } = require('./src/middlewares/middleware')
 
 // Rotas da home
@@ -13,5 +14,6 @@ route.post('/login/register', loginController.register)
 route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
 
+route.get('/api', apiController.index)
 
 module.exports = route;
